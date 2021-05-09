b1b4bdf6216bf756b909419b320ec01d1c5c3587
loc le <tienloc.le2604@gmail.com>
2021-05-09 23:40:59 +0700

    add code editor component

---
+++ src/components/CodeEditor/CodeEditor.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-c_cpp";
+import "ace-builds/src-noconflict/theme-monokai";
+
+const initCode =
+  '#include <iostream>\nusing namespace std;\nint main(){\n\tcout<< "Hello World";\n\treturn 0; \n}';
+
+const CodeEditor = () => {
+  const [sourceCode, setSourceCode] = useState(initCode);
+  return (
+    <AceEditor
+      placeholder="Enter your code here"
+      mode="c_cpp"
+      theme="monokai"
+      name="blah2"
+      onChange={setSourceCode}
+      fontSize={18}
+      showPrintMargin={true}
+      showGutter={true}
+      highlightActiveLine={true}
+      value={sourceCode}
+      setOptions={{
+        enableBasicAutocompletion: true,
+        enableLiveAutocompletion: true,
+        enableSnippets: true,
+        showLineNumbers: true,
+        tabSize: 2,
+      }}
+    />
+  );
+};
+export default CodeEditor;
