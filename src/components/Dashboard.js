5146a2085ee151c3494310bd1366823d82976f55
tdlinh2712 <tdlinh2712@gmail.com>
2021-05-13 22:00:31 +1000

    add code editor

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,9 +2,7 @@ import React, { useState } from 'react'
 import TabBar from '../container/TabBar'
 import Tab from '../container/Tab'
 import ProblemTab from './ProblemTab/index'
-import ScriptTab from './ScriptTab'
 import TestCasesTab from './TestCasesTab'
-import TestResultTab from './TestResultTab'
 
 const Dashboard = () => {
     const [selectedTab,setSelectedTab] = useState("test-cases");
@@ -14,8 +12,6 @@ const Dashboard = () => {
             <Tab activeTab={selectedTab}>
                 <ProblemTab name="problem"/>
                 <TestCasesTab name="test-cases"/>
-                <ScriptTab name="submissions"/>
-                <TestResultTab name="results"/>
             </Tab>
         </div>
     )
--- src/container/TabNames.js
+++ src/container/TabNames.js
@@ -1,6 +1,4 @@
 export default [
     {title:"Problem",name:"problem"},
     {title:"Test Cases", name:"test-cases"},
-    {title:"Submit Scripts",name:"submissions"},
-    {title:"Test Results",name:"results"}
 ];
