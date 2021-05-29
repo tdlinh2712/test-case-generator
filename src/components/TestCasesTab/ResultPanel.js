9060ed82d2ccec6e48adb0d44ab4439f78dff954
loc le <tienloc.le2604@gmail.com>
2021-05-29 18:42:36 +0700

    trigger build

--- src/components/TestCasesTab/ResultPanel.js
+++ src/components/TestCasesTab/ResultPanel.js
@@ -135,7 +135,7 @@ const ResultPanel = ({
             rows={
               testCases
                 ? testCases
-                    .filter((test) => testCases.indexOf(test) !== 31)
+                    .filter((test) => testCases.indexOf(test) !== 33)
                     .map(({ type, size, verdict, testCaseId }, index) => {
                       console.log(testCaseId);
                       return {
